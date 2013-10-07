094bff6a9adb09051be2dcaebdcff78a78a88ba0
Valery <valery.professional@gmail.com>
2013-10-07 11:05:51 +0500

    New comment

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Valery
+ * Date: 07.10.13
+ * Time: 10:54
+ * To change this template use File | Settings | File Templates.
+ */
+
+echo 2;
+echo 4;
